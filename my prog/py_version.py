43f868332fc4d825ef9f995c017e9a1cdc4206e3
SAN06311521 <47052719+SAN06311521@users.noreply.github.com>
2019-06-25 00:53:14 +0530

    Create py_version.py

---
+++ my prog/py_version.py
@@ -0,0 +1,6 @@
+import sys
+print("the python version installed in the laptop is: ")
+print(sys.version)
+print("\n")
+print("the information about the installed version is : ")
+print(sys.version_info)
