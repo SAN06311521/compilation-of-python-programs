949cb962fa6ede9d9e113cf541628d82a2141c92
SAN06311521 <47052719+SAN06311521@users.noreply.github.com>
2019-06-24 23:21:51 +0530

    Create area_rect.py

---
+++ my prog/area_rect.py
@@ -0,0 +1,8 @@
+# the program determine the area and perimeter of rectangle
+
+l = float(input("Enter the length of the rectangle: "))
+b = float(input("Enter the breath of the rectangle: "))
+a = l*b
+p = 2*(l + b)
+print("The area of rectangle is {}".format(a))
+print("The perimeter of rectangle is {}".format(p))
